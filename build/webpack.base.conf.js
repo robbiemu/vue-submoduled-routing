273722c8bf6f1b471049cad25409677cc5a6d493
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2018-01-18 11:17:33 -0500

    initial conf

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -27,6 +27,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+	  '@common': resolve('vue-submoduled-routing-common/src')	
     }
   },
   module: {
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import common from '@common/router'
+import Main from '@/components/Main'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/main',
+      name: 'main',
+      component: Main
+    },
+	...common.routes
   ]
 })
